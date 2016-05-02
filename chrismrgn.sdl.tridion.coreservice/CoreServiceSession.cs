278883888583c7626d95be5d8ea4955fa1864a22
Chris Morgan <chris@mrgn.co>
2016-05-02 09:59:14 -0700

    initial version

---
+++ chrismrgn.sdl.tridion.coreservice/CoreServiceSession.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Net;
+using System.ServiceModel;
+using Tridion.ContentManager.CoreService.Client;
+
+namespace chrismrgn.sdl.tridion.coreservice
+{
+    public class CoreServiceSession : IDisposable
+    {
+        private string _coreServiceVersion;
+
+        private CoreServiceClient _client;
+
+        public CoreServiceSession()
+        {
+            InitializeClient("CoreService", CredentialCache.DefaultNetworkCredentials);
+        }
+
+        public CoreServiceSession(string endPoint, NetworkCredential credentials, bool useSessionAware = false)
+        {
+            if(useSessionAware)
+                InitializeSessionAwareClient(endPoint, credentials);
+            else
+                InitializeClient(endPoint, credentials);
+        }
+
+        private void InitializeClient(string endPoint, NetworkCredential credentials)
+        {
+            try
+            {
+                _client = new CoreServiceClient(endPoint);
+                _client.ChannelFactory.Credentials.Windows.ClientCredential = credentials;
+                
+                if (_client != null) _coreServiceVersion = _client.GetApiVersion();
+            }
+
+            catch (EndpointNotFoundException e) { }
+            catch (Exception e) { }
+        }
+
+        private void InitializeSessionAwareClient(string endPoint, NetworkCredential credentials)
+        {
+            try
+            {
+                var _client = new SessionAwareCoreServiceClient(endPoint);
+                _client.ChannelFactory.Credentials.Windows.ClientCredential = credentials;
+
+                if (_client != null) _coreServiceVersion = _client.GetApiVersion();
+            }
+
+            catch (EndpointNotFoundException e) { }
+            catch (Exception e) { }
+        }
+
+        public CoreServiceClient CoreServiceClient
+        {
+            get { return _client; }
+        }
+
+        public UserData User
+        {
+            get { return _client.GetCurrentUser(); }
+        }
+
+        public string CoreServiceVersion
+        {
+            get { return _coreServiceVersion; }
+        }
+
+        public void Dispose()
+        {
+            if (_client.State == CommunicationState.Faulted)
+            {
+                _client.Abort();
+            }
+            else
+            {
+                _client.Close();
+            }
+        }
+    }
+}
---
+++ chrismrgn.sdl.tridion.coreservice/TridionCoreServiceFactory.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using chrismrgn.sdl.tridion.coreservice.Extensions;
+using Tridion.ContentManager.CoreService.Client;
+
+namespace chrismrgn.sdl.tridion.coreservice
+{
+    public static class TridionCoreServiceFactory
+    {
+        public static CoreServiceClient CreateCoreService()
+        {
+            var endPoint = ConfigurationManager.AppSettings["EndPoint"] ?? "CoreService";
+            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["Username"] ?? "DOMAIN\\USERNAME",
+                                                        ConfigurationManager.AppSettings["Password"] ?? "PASSWORD");
+            return new CoreServiceSession(endPoint, credentials).CoreServiceClient;
+        }
+
+        public static PublishTransactionData[] Publish(string [] itemIds, string[] targets, PublishInstructionData publishInstruction = null, PublishPriority priority = PublishPriority.Normal, ReadOptions readOptions = null)
+        {
+            PublishTransactionData[] obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    if (readOptions == null) readOptions =new ReadOptions { LoadFlags = LoadFlags.Expanded }; ;
+                    if (publishInstruction == null) publishInstruction = new PublishInstructionData { };;
+
+
+                    if (readOptions == null) readOptions = new ReadOptions { LoadFlags = LoadFlags.Expanded };
+                    obj = client.Publish(itemIds, publishInstruction, targets, priority, readOptions);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj;
+        }
+
+        public static T Get<T>(string id, ReadOptions readOptions = null) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    if (readOptions == null) readOptions = new ReadOptions { LoadFlags = LoadFlags.Expanded };
+                    obj = client.Read(id, readOptions);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetSchemaFields<T>(string id, ReadOptions readOptions = null) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    if (readOptions == null) readOptions = new ReadOptions { LoadFlags = LoadFlags.Expanded };
+                    obj = client.ReadSchemaFields(id, true, readOptions);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetSearchResults<T>(SearchQueryData filter) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetSearchResults(filter);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetSystemWideList<T>(SystemWideListFilterData filter) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetSystemWideList(filter);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetSystemWideListXml<T>(SystemWideListFilterData filter) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetSystemWideListXml(filter);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetCurrentUser<T>() where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetCurrentUser();
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetList<T>(string id, SubjectRelatedListFilterData filter) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetList(id, filter);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                    //Console.WriteLine(e.Message);
+                }
+            });
+            return obj as T;
+        }
+
+        public static T GetListXml<T>(string id, SubjectRelatedListFilterData filter) where T : class
+        {
+            object obj = null;
+            CreateCoreService().Using(client =>
+            {
+                try
+                {
+                    obj = client.GetListXml(id, filter);
+                }
+                catch (Exception e)
+                {
+                    obj = null;
+                }
+            });
+            return obj as T;
+        }
+
+        //Implement other exposed client methods here
+    }
+}
