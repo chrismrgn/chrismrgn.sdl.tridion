1a1b8f3ec2e7f4082364fd7c2834313fd9392ea0
Chris Morgan <SESA398513@schneider-electric.com>
2016-05-30 19:21:57 +0200

    opimization

--- Schneider.SchemaUsageExport/Program.cs
+++ Schneider.SchemaUsageExport/Program.cs
@@ -2,6 +2,7 @@
 using chrismrgn.sdl.tridion.core.Logging;
 using chrismrgn.sdl.tridion.coreservice.Helpers;
 using System;
+using System.Linq;
 using Tridion.ContentManager.CoreService.Client;
 
 namespace Schneider.SchemaUsageExport
@@ -19,34 +20,32 @@ namespace Schneider.SchemaUsageExport
 
         private static void ProcessSchemasAndUsage()
         {
+            var schemas = ItemHelpers.LoadAllByPublication<SchemaData>();
+
             Logger.For(typeof(Program)).Info("Multimedia Schemas");
             Logger.For(typeof(Program)).Info("====================");
-            var schemas = ItemHelpers.LoadAllByPublication<SchemaData>(schemaPurposesToInclude: new SchemaPurpose[] { SchemaPurpose.Multimedia });
-            foreach (var schema in schemas)
+            foreach (var schema in schemas.Where(x=>x.Purpose == SchemaPurpose.Multimedia))
             {
                 Logger.For(typeof(Program)).InfoFormat("Schema {0} used {1} times", schema.Title, schema.GetAllUsageCount<ComponentData>());
             }
 
             Logger.For(typeof(Program)).Info("Content Schemas");
             Logger.For(typeof(Program)).Info("====================");
-            schemas = ItemHelpers.LoadAllByPublication<SchemaData>(schemaPurposesToInclude: new SchemaPurpose[] { SchemaPurpose.Component });
-            foreach (var schema in schemas)
+            foreach (var schema in schemas.Where(x => x.Purpose == SchemaPurpose.Component))
             {
                 Logger.For(typeof(Program)).InfoFormat("Schema {0} used {1} times", schema.Title, schema.GetAllUsageCount<ComponentData>());
             }
 
             Logger.For(typeof(Program)).Info("Embedded Schemas");
             Logger.For(typeof(Program)).Info("====================");
-            schemas = ItemHelpers.LoadAllByPublication<SchemaData>(schemaPurposesToInclude: new SchemaPurpose[] { SchemaPurpose.Embedded });
-            foreach (var schema in schemas)
+            foreach (var schema in schemas.Where(x => x.Purpose == SchemaPurpose.Embedded))
             {
                 Logger.For(typeof(Program)).InfoFormat("Schema {0} used {1} times", schema.Title, schema.GetAllUsageCount<ComponentData>());
             }
 
             Logger.For(typeof(Program)).Info("Metadata Schemas");
             Logger.For(typeof(Program)).Info("====================");
-            schemas = ItemHelpers.LoadAllByPublication<SchemaData>(schemaPurposesToInclude: new SchemaPurpose[] { SchemaPurpose.Metadata });
-            foreach (var schema in schemas)
+            foreach (var schema in schemas.Where(x => x.Purpose == SchemaPurpose.Metadata))
             {
                 Logger.For(typeof(Program)).InfoFormat("Schema {0} used {1} times", schema.Title, schema.GetAllUsageCount<ComponentData>());
             }
